fcdf2d104be2558c03152d60b1dc538735632e2f
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-05-15 07:38:29 -0700

    remove c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+#def no_c(my_string):
+#    newStr = {99:None}
+#    nz = {67:None}
+#    newest =  my_string.translate(newStr)
+#    rsStr = newest.translate(nz)
+#    return rsStr
 def no_c(my_string):
-    newStr = {99:None}
-    nz = {67:None}
-    newest =  my_string.translate(newStr)
-    rsStr = newest.translate(nz)
-    return rsStr
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
