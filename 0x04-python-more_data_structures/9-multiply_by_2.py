c9cf03434c9af73dbd87d463080934246028175d
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-05-17 21:46:49 +0300

    hot fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     #holder = []
-    #listOfValues = list(a_dictionary.values())
-    #for i in listOfValues:
-       #holder.append(i * 2)
-    #newDict = dict(zip(list(a_dictionary.keys()), holder))
-    #return newDict
-    new_dictionary = {}
-    for key in a_dictionary:
-        new_dictionary.update({key: a_dictionary.get(key) * 2})
-    return new_dictionary
+    listOfValues = list(a_dictionary.values())
+    for i in listOfValues:
+       holder.append(i * 2)
+    newDict = dict(zip(list(a_dictionary.keys()), holder))
+    return newDict
