efc25c424d70baacb5f8efd4cf00e5c2546344e8
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-05-17 10:52:19 -0700

    print all dic items

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i, j in dict(sorted(a_dictionary.items())).items():
-        print("{:s} : {}".format(str(i), j))
+        print("{:s} : {}".format(str(i), str(j)))
