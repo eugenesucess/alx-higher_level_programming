8d6ab028a8e151e6da60683c8a87dd072abdadf0
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-05-17 11:17:14 -0700

    delete a key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if str(key) not in a_dictionary:
+        return a_dictionary
+    else:
+        del a_dictionary[str(key)]
+        return a_dictionary
