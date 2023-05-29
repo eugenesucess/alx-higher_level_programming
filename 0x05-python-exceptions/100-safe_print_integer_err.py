170263cc79a3186af246bc222ccf38f7f6f5e12f
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-05-29 14:26:04 -0700

    print at stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 import sys
-def safe_print_integer(value):
+
+
+def safe_print_integer_err(value):
     try:
-        sys.stderr.write("{:d}".format(value))
-        print("\n")
+        print("{:d}".format(value))
     except (TypeError, ValueError):
+        sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
         return False
     return True
