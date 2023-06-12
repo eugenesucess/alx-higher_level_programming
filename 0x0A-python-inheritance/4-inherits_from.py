79ea4160a707e6fe69a1cead0ca6475b910416c4
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-12 20:52:56 +0300

    inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ shows name of class that it inherits from
 
 def inherits_from(obj, a_class):
     """check if class has inherited frm super class"""
-    return issubclass(type(obj), a_class) and type(obj) != a_class:
+    return issubclass(type(obj), a_class) and type(obj) != a_class
