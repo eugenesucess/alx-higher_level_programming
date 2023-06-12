38fd92f7b17e4601671b1ec0b30fb2c09699d3aa
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-06-12 11:10:09 -0700

    create non implemented class instance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,5 +6,5 @@ Class Called BaseGeometry
 class BaseGeometry:
     """This is a class called BaseGeometry"""
     def area(self):
-         """This Function Raise an Error"""
-        raise Exception("area() is not implemented\n")
+        """This Function Raise an Error"""
+        raise Exception("area() is not implemented")
