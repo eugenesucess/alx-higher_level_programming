8e59588521a61079f52827801cf18b75eabb7090
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-06-12 12:00:43 -0700

    creates rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,9 @@ inherit BaseGeometry class
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    def __int__(self, width, height):
+    """ectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
         """Instantiation of a new Rectangle"""
         self.integer_validator("width", width)
         self.__width = width
