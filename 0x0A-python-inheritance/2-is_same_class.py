788f457cfe91ad98351d2db01e807e60707f47fd
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-06-12 09:55:18 -0700

    check same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
-    """ returns true if the object passed as arg is the same type as the class"""
-    if isintance(obj, a_class):
+    """ returns true if the object passed as arg is the same type"""
+    if type(obj) is a_class:
         return True
     return False
