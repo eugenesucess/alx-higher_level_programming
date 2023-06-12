021e0ad6b07aec307a3270072fba374feecd041c
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-06-12 07:29:19 -0700

    print sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,11 +5,5 @@ MyClass class inherit from list class
 
 
 class MyList(list):
-    def print_sprted(self):
-        for i in range(len(list)):
-            if not isinstance(list[i], int):
-                return None
-             if list[i] > list[i + 1]:
-                  bigN = list[i]
-                  list[i] = list[i + 1]
-                  list[i + 1] = bigN
+    def print_sorted(self):
+        print(sorted(self))
