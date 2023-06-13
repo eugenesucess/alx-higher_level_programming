4b692ba6a9bd1e02f7e08425f601a9fac2037f36
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-13 17:18:41 +0300

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,16 @@
 add all arguments to the list
 """
 import sys
+if __name__ == "__main__":
+        args = sys.argv
+        save_to_json_file = \
+            __import__('5-save_to_json_file').save_to_json_file
+        load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = \
-    __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = \
-    __import__('6-load_from_json_file').load_from_json_file
-
-args = sys.argv
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(args[1:])
-save_to_json_file(items, "add_item.json")
+        try:
+            items = load_from_json_file("add_item.json")
+        except FileNotFoundError:
+            items = []
+        items.extend(args[1:])
+        save_to_json_file(items, "add_item.json")
