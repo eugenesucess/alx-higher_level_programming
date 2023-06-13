ae35aad94a49c11cb9472064a991643d866269ec
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-13 17:21:54 +0300

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,6 @@ add all arguments to the list
 """
 import sys
 if __name__ == "__main__":
-        args = sys.argv
         save_to_json_file = \
             __import__('5-save_to_json_file').save_to_json_file
         load_from_json_file = \
@@ -14,5 +13,5 @@ if __name__ == "__main__":
             items = load_from_json_file("add_item.json")
         except FileNotFoundError:
             items = []
-        items.extend(args[1:])
+        items.extend(sys.argv[1:])
         save_to_json_file(items, "add_item.json")
