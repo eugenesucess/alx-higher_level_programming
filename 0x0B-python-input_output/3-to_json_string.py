7d78e2163fc0480888469b20ed362ff009312faf
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-13 17:07:37 +0300

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+change python object format to json format
+"""
+
+
+def to_json_string(my_obj):
+    """change format to the json"""
+    return json.dumps(my_obj)
