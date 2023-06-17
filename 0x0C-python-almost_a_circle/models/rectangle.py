b8dfe35e82908d7163dd871f436fbafd68b15be1
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-17 16:57:22 +0300

    changing error on variable nae

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,9 +20,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, valuePased):
         if type(valuePased) != int:
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         if valuePased <= 0:
-            raise ValueError("Width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = valuePased
     
     @property
@@ -32,9 +32,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, valuePased):
         if type(valuePased) != int:
-            raise TypeError("Height must be an integer")
+            raise TypeError("height must be an integer")
         if valuePased <= 0:
-            raise ValueError("Height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = valuePased
     
     
@@ -57,7 +57,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, valuePased):
         if type(valuePased) != int:
-            raise TypeError("Width must be an integer")
+            raise TypeError("yidth must be an integer")
         if valuePased < 0:
             raise TypeError("y must be >= 0")
         self.__y = valuePased
