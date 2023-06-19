429a92d30aa024d947dea10e69da8642e2e96be5
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-06-19 20:51:20 +0300

    square class tester

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -607,8 +607,8 @@ class TestSquare_update_kwargs(unittest.TestCase):
 
     def test_update_args_and_kwargs(self):
         s = Square(10, 10, 10, 10)
-        s.update(89, 2, y=6)
-        self.assertEqual("[Square] (89) 10/10 - 2", str(s))
+        s.update(89, 10, y=6)
+        self.assertEqual("[Square] (89) 10/10 - 10", str(s))
 
     def test_update_kwargs_some_wrong_keys(self):
         s = Square(10, 10, 10, 10)
